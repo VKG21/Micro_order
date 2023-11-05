d58a1f9e93e95c59975630a848a00642a8e2eca7
Admin <Admin@DESKTOP-DIN09ET>
2023-11-05 23:12:22 +0530

    done profile annotation envoirement specific file by using dev for in config server created folder dev and provided file property

--- OrderService/src/main/java/com/order/OrderService/OrderServiceApplication.java
+++ OrderService/src/main/java/com/order/OrderService/OrderServiceApplication.java
@@ -3,6 +3,9 @@ package com.order.OrderService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -13,4 +16,11 @@ public class OrderServiceApplication {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
+	
+	@Bean
+	@Profile("dev")
+	public RestTemplate restTemplate() {
+		System.out.println("bean is initillized for dev envoirement in java");
+		return new RestTemplate();
+	}
 }
